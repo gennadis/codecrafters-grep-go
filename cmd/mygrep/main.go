3c8ebfb8c74f6272e4a89ea848896357fc20235e
Геннадий Бадинов <ggbadinov@sc-mac-01308.corp.sbercloud.ru>
2024-09-12 16:00:04 +0300

    step 1: match line

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -8,9 +8,6 @@ import (
 	"unicode/utf8"
 )
 
-// Ensures gofmt doesn't remove the "bytes" import above (feel free to remove this!)
-var _ = bytes.ContainsAny
-
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 func main() {
 	if len(os.Args) < 3 || os.Args[1] != "-E" {
@@ -43,14 +40,7 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	if utf8.RuneCountInString(pattern) != 1 {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
-
 	var ok bool
-
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this to pass the first stage
-	// ok = bytes.ContainsAny(line, pattern)
-
+	ok = bytes.ContainsAny(line, pattern)
 	return ok, nil
 }
